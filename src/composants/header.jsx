526680a58b2871502a43b1d56e39e656284bd891
EMMADEKO10 <emmanueldeko64@gmail.com>
2023-11-30 15:21:28 +0100

    Ajout des fichiers

---
+++ src/composants/header.jsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+     return(
+        <header>
+            
+            <h1>My Team</h1>
+            <ul>
+                <li>Professeurs</li>
+                <li>Etudiants</li>
+                <li>Programmes</li>
+                <li>Administration</li>
+            </ul>
+            
+        </header>
+     );
+}
---
+++ src/composants/panelRubric.jsx
@@ -0,0 +1,8 @@
+export default function Panel(){
+    return(
+        <div className="panel">
+            <div className="panel-professeur">Panel Professeur</div>
+            <div className="panel-etudiant">Panel etudiant</div>
+        </div>
+    )
+}
